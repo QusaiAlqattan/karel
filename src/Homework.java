dea463008089ae686bffd8c64133eee26a079fd2
QusaiAlqattan <qusaikattan.jb@gmail.com>
2024-06-08 18:34:22 +0300

    first after finish, before testing

--- src/Homework.java
+++ src/Homework.java
@@ -7,12 +7,14 @@ public class Homework extends SuperKarel {
     private int h;
     private int total_count;
     private int jump;
+    private boolean is_extra;
 
     /* You fill the code here */
     public void run() {
         w = 1;
         h = 1;
         total_count = 0;
+        is_extra = false;
 
         while (!facingEast()){
             turnRight();
@@ -23,6 +25,9 @@ public class Homework extends SuperKarel {
         int state = select_state();
         System.out.println("w: " + w);
         System.out.println("h: " + h);
+        System.out.print("jump: ");
+        System.out.println(jump);
+        System.out.print("state: ");
         System.out.println(state);
 
         // divide based on the state
@@ -39,9 +44,14 @@ public class Homework extends SuperKarel {
             single_lines('w');
             single_lines('h');
         } else if (state == 4 || state == 5 || state == 6) {
-            with_jump('w', jump);
+            with_jump('w');
         } else if (state == 7 || state == 8 || state == 9) {
-            with_jump('h', jump);
+            turnLeft();
+            with_jump('h');
+        } else if (w == 2 & h == 2) {
+            zig_zag();
+        } else {
+            System.out.println(("Karel: Sorry, can't be divided"));
         }
     }
 
@@ -68,28 +78,46 @@ public class Homework extends SuperKarel {
             state = 1;
         }else if (w <= 2 || h <= 2) {
             if (w > h){
-                if((w - 3) % 4 == 0){
+                if((w - 3 != 0) && ((w - 3) % 4 == 0) || ( (w - 4 != 0) && (w - 4) % 4 == 0)){
                     state = 4;
                     jump = (w - 3) / 4;
-                }else if ((w - 2) % 3 == 0) {
+                    if ((w - 4 != 0) && (w - 4) % 4 == 0){
+                        is_extra = true;
+                    }
+                }else if (((w - 2) % 3 == 0) || ( (w - 3 != 0) &&  (w - 3) % 3 == 0)) {
                     state = 5;
                     jump = (w - 2) / 3;
-                }else if ((w - 1) % 2 == 0) {
+                    if ((w - 3 != 0) &&  (w - 3) % 3 == 0){
+                        is_extra = true;
+                    }
+                }else if (((w - 1) % 2 == 0) || ((w - 2) % 2 == 0)) {
                     state = 6;
                     jump = (w - 1) / 2;
+                    if ((w - 2) % 2 == 0){
+                        is_extra = true;
+                    }
                 }else{
                     state = 1;
                 }
             }else{
-                if((h - 3) % 4 == 0){
+                if((h - 3 != 0) && ((h - 3) % 4 == 0) || ( (h - 4 != 0) && (h - 4) % 4 == 0)){
                     state = 7;
                     jump = (h - 3) / 4;
-                }else if ((h - 2) % 3 == 0) {
+                    if ((h - 4 != 0) && (h - 4) % 4 == 0){
+                        is_extra = true;
+                    }
+                }else if (((h - 2) % 3 == 0) || ( (h - 3 != 0) &&  (h - 3) % 3 == 0)) {
                     state = 8;
                     jump = (h - 2) / 3;
-                }else if ((h - 1) % 2 == 0) {
+                    if ((h - 3 != 0) &&  (h - 3) % 3 == 0){
+                        is_extra = true;
+                    }
+                }else if (((h - 1) % 2 == 0) || ((h - 2) % 2 == 0)) {
                     state = 9;
                     jump = (h - 1) / 2;
+                    if ((h - 2) % 2 == 0){
+                        is_extra = true;
+                    }
                 }else{
                     state = 1;
                 }
@@ -206,39 +234,39 @@ public class Homework extends SuperKarel {
     }
 
     // fill with jump
-    private void with_jump(char z, int j){
-        if (z == 'w'){
-            int x = 1;
-            while (x++ <= (w - jump)) {
-                int count = 0;
-                while (count++ < jump) {
-                    move_n_no_beeper();
-                }
-                putBeeper();
-            }
-        } else if (z == 'h') {
-
+    private void with_jump(char z){
+        int x = 1;
+        int a;
+        if ( z == 'w') {
+            a = w;
+        }else{
+            a = h;
         }
+        while (x <= (a - (jump + 1 ))) {
+            int count = 0;
+            while (count++ < jump) {
+                move_n_no_beeper();
+                x++;
+            }
+            putBeeper();
+            move_n_no_beeper();
+            x++;
+        }
+        if(is_extra){
+            int cout = 0;
+            while (cout++ < jump)
+                move_n_no_beeper();
+            putBeeper();
+        }
+        return_to_origin();
     }
-//    private void r_zig_zag(){
-//        move();
-//        System.out.println(++total_count);
-//        turnLeft();
-//        move();
-//        System.out.println(++total_count);
-//        if (noBeepersPresent())
-//        putBeeper();
-//        turnRight();
-//    }
-//
-//    private void l_zig_zag(){
-//        move();
-//        System.out.println(++total_count);
-//        turnRight();
-//        move();
-//        System.out.println(++total_count);
-//        if (noBeepersPresent())
-//        putBeeper();
-//        turnLeft();
-//    }
+
+    // zig zag for 2x2
+    private void zig_zag(){
+        putBeeper();
+        move_n_no_beeper();
+        turnLeft();
+        move_n_beeper();
+        return_to_origin();
+    }
 }
--- worlds/8x1.w
+++ worlds/8x1.w
@@ -1,1 +1,5 @@
-Dimension: (8, 1)Beeper: (1, 1) 1Beeper: (2, 1) 1Beeper: (8, 1) 1BeeperBag: INFINITYKarel: (1, 1) EastSpeed: 0
+Dimension: (8, 1)
+Karel: (1, 1) east
+
+BeeperBag: INFINITE
+Speed: 0.00
